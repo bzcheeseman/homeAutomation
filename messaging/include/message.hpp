27b3d3e399dc6421cc82b30bbef993ca83a381d8
Aman <alachapelle@uchicago.edu>
2017-01-24 12:46:07 -0600

    added stream operators

--- messaging/include/message.hpp
+++ messaging/include/message.hpp
@@ -31,37 +31,57 @@
 
 #include <dlib/serialize.h>
 
-class message {
+#include <zmqpp/zmqpp.hpp>
 
-  const std::string recipient;
-  const std::string sender;
-  std::vector<std::string> contents;
+#include "dlib_zmq.hpp"
 
-public:
-  message(const std::string &recipient, const std::string &sender) : recipient(recipient), sender(sender) {}
+namespace messaging {
 
-  //Use this to set source and destination for zmq.
-  //Guarantees that the recipient and sender for a given message cannot change.
-  const std::string &get_recipient() const { return recipient; }
-  const std::string &get_sender() const { return sender; }
+  class message {
+  
+    const std::string recipient;
+    const std::string sender;
+    std::vector<std::string> contents;
+  
+  public:
+    message(const std::string &recipient, 
+            const std::string &sender) : recipient(recipient), sender(sender) {}
+  
+    //Use this to set source and destination for zmq.
+    //Guarantees that the recipient and sender for a given message cannot change.
+    const std::string &get_recipient() const { return recipient; }
+    const std::string &get_sender() const { return sender; }
+  
+    //Modifies the contents of the message
+    inline void add(std::string payload){ contents.push_back(payload); }
+    inline std::string &get(int which){ return contents[which]; }
+    inline void remove(int which){ 
+      auto iter_which = contents.begin() + which; 
+      contents.erase(iter_which); 
+    }
+  
+    //If there's only one item in the message, we can access it directly as a string if we so choose.
+    inline std::string &str(){ assert(contents.size() == 1); return contents[0]; }
+  
+    //Note these are do NOT operate on a heap data structure, just thought they might come in handy.
+    inline void push(std::string payload){ this->add(payload); }
+    inline std::string &pop(){ return *(contents.end()); }
+  
+    //Serialize a message to send over a zmq socket. The ostream can be a stringstream or something, that's kosher.
+    //Note that this ONLY sends the contents, recipient/sender needs to be taken care of outside of this.
+    friend zmqpp::message &operator>>(zmqpp::message &out, message &msg){ //check on these insertion operators
+      dlib_zmq::to_zmq(msg.contents, out);
+      return out;
+    }
+    
+    friend message operator<<(message &out, zmqpp::message &msg){ //check on these insertion operators
+      dlib_zmq::from_zmq(out.contents(), msg);
+      return out;
+    }
+  
+  };
 
-  //Modifies the contents of the message
-  inline void add(std::string payload){ contents.push_back(payload); }
-  inline std::string &get(int which){ return contents[which]; }
-  inline void remove(int which){ auto iter_which = contents.begin() + which; contents.erase(iter_which); }
-
-  //If there's only one item in the message, we can access it directly as a string if we so choose.
-  inline std::string &str(){ assert(contents.size() == 1); return contents[0]; }
-
-  //Note these are do NOT operate on a heap data structure, just thought they might come in handy.
-  inline void push(std::string payload){ this->add(payload); }
-  inline std::string &pop(){ return *(contents.end()); }
-
-  //Serialize a message to send over a zmq socket. The ostream can be a stringstream or something, that's kosher.
-  //Note that this ONLY sends the contents, recipient/sender needs to be taken care of outside of this.
-  friend bool serialize(message &message, std::ostream &out){ dlib::serialize(message.contents, out); } //maybe return a zmq message?
-
-};
+}; //messaging
 
 
 #endif //HOMEAUTOMATION_MESSAGE_HPP
